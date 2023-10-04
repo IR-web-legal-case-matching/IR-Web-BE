6e7c9580b4e4abd8b98f728461e7c0f5669960b8
ce-amtic <fyh516@126.com>
2023-10-04 21:41:29 +0800

    feat: ready to release

--- IRwebBE/settings.py
+++ IRwebBE/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_j^&f*$fx7$^h5e-*bs2y*oq3d78#@uybc=8_*q9qsq&5uj@1u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
